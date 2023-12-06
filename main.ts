4654eaf1b4b9d8846bb8577435167427a4520920
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-12-06 12:44:17 -0500

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Angelo Yalung
+ * Created on: Dec 2023
+ * This program receives radio signals and changes the message depending on the distance of a sensor
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+
+// setup
+radio.setGroup(76)
+basic.showIcon(IconNames.Happy)
+
+while (true) {
+  distanceToObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+
+    if (distanceToObject <= 10) {
+      radio.sendString('Too Close')
+    } else {
+      radio.sendString('Pee')
+    }
+
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
+}
